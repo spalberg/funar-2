2da83d3f06b68c134d4a114e20c926d2eec5335a
Johannes Maier <johannes.maier@active-group.de>
2023-01-16 10:22:09 +0100

    update

--- vorkurs/animal.rkt
+++ vorkurs/animal.rkt
@@ -1,1 +1,37 @@
 #lang deinprogramm/sdp/beginner
+
+; Datenanalyse
+; Datendefinition -> wir beschreiben die Dinge natürlichsprachlich
+
+; Haustier
+; - Katze
+; - Hund
+; - Schlange
+
+; besser: Haustier _ist eins der Folgenden_:
+; - Katze ODER
+; - Hund ODER
+; - Schlange
+(define pet
+  (signature (enum "cat" "dog" "snake")))
+
+; Konstruktionsanleitung
+#;(define cute? ; sprich: cute-p
+  (lambda (pet)
+    ...))
+
+; Schablone:
+; bei Daten, die Fallunterscheidung sind: Fälle unterschiedlich behandeln
+(define cute? ; sprich: cute-p
+  (lambda (pet)
+    (cond
+      ((string=? pet "cat") #t) ; (<Bedingung> <Ergebnis>)
+      ((string=? pet "dog") #t)
+      ((string=? pet "snake") #f))))
+
+(check-expect (cute? "snake")
+              #f)
+(check-expect (cute? "cat")
+              #t)
+(check-expect (cute? "dog")
+              #t)
