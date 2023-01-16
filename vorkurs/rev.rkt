0a7d84c2865e48bf2d25fa4666c25c6b471b5934
Johannes Maier <johannes.maier@active-group.de>
2023-01-16 16:25:22 +0100

    update

--- vorkurs/rev.rkt
+++ vorkurs/rev.rkt
@@ -5,6 +5,8 @@
 
 (check-expect (rev (list 1 2 3 4))
               (list 4 3 2 1))
+(check-expect (rev empty)
+              empty)
 
 (define rev
   (lambda (list)
@@ -12,7 +14,40 @@
       ((empty? list) empty)
       ((cons? list)
        #;(cons (rev (rest list)) (first list))
-       (cons (first list) (rev (rest list)))))))
+       #;(cons (first list) (rev (rest list)))
+       (add-element (rev (rest list))
+                    (first list))))))
+
+; 1 + 2 + 3 + 4 + ... + (n-1) + n -> n*(n+1)/2 -> O(n^2)
 
 ; Element an Liste _hinten_ anhängen
 (: add-element ((list-of %a) %a -> (list-of %a)))
+
+(check-expect (add-element empty 3)
+              (list 3))
+(check-expect (add-element (list 2 3) 4)
+              (list 2 3 4))
+  
+(define add-element
+  (lambda (xs element)
+    (if (empty? xs)
+        (list element)
+        (cons
+         (first xs)
+         (add-element (rest xs) element)))))
+
+#;(add-element (list 2 3) 4)
+#;(rev (list 2 3 4))
+
+(define rev*
+  (lambda (list acc) ; Akkumulator ->
+    ; Liste der gesehenen Elemente (umgedreht)
+    (cond
+      ((empty? list) acc)
+      ((cons? list)
+       (rev* (rest list)
+             (cons (first list)
+                   acc))))))
+
+
+
