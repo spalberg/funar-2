cbaee212c8a2ca86bd5e113d6896cbab1d492e33
Johannes Maier <johannes.maier@active-group.de>
2023-01-17 11:21:59 +0100

    Update

--- johannes/haskell-code/Intro.hs
+++ johannes/haskell-code/Intro.hs
@@ -60,9 +60,9 @@ type Weight = Integer
 -- Record
 --   v Typkonstruktor
 --            v Datenkonstruktor
-data Dillo = MkDillo { dilloLiveness :: Liveness,
-                       dilloWeight :: Weight }
-    deriving Show
+-- data Dillo = MkDillo { dilloLiveness :: Liveness,
+--                        dilloWeight :: Weight }
+--     deriving Show
 
 dillo1 = MkDillo Alive 5
 dillo3 = MkDillo Alive
@@ -71,7 +71,7 @@ dillo3 = MkDillo Alive
 -- Reihenfolge egal
 dillo2 = MkDillo { dilloLiveness = Dead, dilloWeight = 8 }
 
-runOverDillo :: Dillo -> Dillo
+-- runOverDillo :: Dillo -> Dillo
 -- variablen: kleingeschrieben
 -- Record-Update-Syntax: gleicher Dillo, mit geändertem Wert
 -- >>> runOverDillo dillo1
@@ -82,5 +82,83 @@ runOverDillo :: Dillo -> Dillo
 -- runOverDillo (MkDillo { dilloWeight = w }) =
 --     MkDillo Dead w
 -- runOverDillo (MkDillo _ w) = MkDillo Dead w
-runOverDillo (MkDillo Alive w) = MkDillo Dead w
-runOverDillo dillo = dillo
+-- runOverDillo (MkDillo Alive w) = MkDillo Dead w
+-- runOverDillo dillo = dillo
+
+-- Algebraische Datentypen
+-- Ein Tier ist eins der folgenden:
+-- - Dillo mit Liveness und Gewicht
+-- - Papagei mit Satz und Gewicht
+
+type Sentence = String
+
+-- alternative Modellierung:
+-- data Dillo = MkOldDillo Liveness Weight
+-- -- MkDillo (MkOldDillo Alive 5)
+
+-- mkNewDillo :: Liveness -> Weight -> Animal
+-- mkNewDillo l w = MkDillo (MkOldDillo l w)
+
+data Animal
+    -- Konstruktoren
+    = MkDillo { dilloLiveness :: Liveness, dilloWeight :: Weight }
+    -- = MkDillo Dillo
+    | MkParrot Sentence Weight
+    deriving Show
+
+parrot1 = MkParrot "hello" 3
+parrot2 = MkParrot "bye" 1
+
+-- Tiere überfahren
+runOverAnimal :: Animal -> Animal
+runOverAnimal (MkDillo _ w) = MkDillo Dead w
+runOverAnimal (MkParrot _ w) = MkParrot "" w
+
+-- Tier füttern
+feedAnimal :: Animal -> (Weight -> Animal)
+-- >>> feedAnimal (MkDillo Alive 2) 3
+-- MkDillo {dilloLiveness = Alive, dilloWeight = 5}
+-- feedAnimal (MkDillo Dead weight) amount = MkDillo Dead weight
+-- feedAnimal (MkDillo Alive weight) amount = MkDillo Alive (weight + amount)
+-- feedAnimal dillo@(MkDillo liveness weight) amount =
+feedAnimal dillo@(MkDillo{}) amount =
+    -- Fallunterscheidung in Haskell
+    case dilloLiveness dillo of
+        Dead -> dillo
+        -- Alive -> MkDillo liveness (weight + amount)
+        -- mit Update-Syntax:
+        Alive -> dillo { dilloWeight = amount + dilloWeight dillo }
+feedAnimal (MkParrot sentence weight) amount = MkParrot sentence (weight + amount)
+
+--              v Tupel aus Weight und Animal
+-- >>> (3, MkDillo Alive 3)
+-- (3,MkDillo {dilloLiveness = Alive, dilloWeight = 3})
+feedAnimal' :: (Animal, Weight) -> Animal
+-- feedAnimal' (animal, w) = feedAnimal animal w
+feedAnimal' = tuplify feedAnimal
+
+-- eingebaut uncurry
+tuplify :: (a -> b -> c) -> ((a, b) -> c)
+-- Pattern Matching in Lambda
+tuplify f = \ (a, b) -> f a b
+
+-- eingebaut als curry + schönfinkeln
+untuplify :: ((a, b) -> c) -> a -> b -> c
+untuplify f = \ a b -> f (a, b)
+-- untuplify f a b = f (a, b)
+-- untuplify f a = \ b -> f (a, b)
+
+multWith :: Int -> Int -> Int
+multWith = (*)
+
+-- Duschprodukte:
+-- - Seife mit ph-Wert
+-- - Shampoo mit Haartyp
+-- - Duschgel (50% Seife, 50% Shampoo)
+
+-- 1. Datenanalyse + Datentyp
+-- 2. Funktion, die den Seifenanteil berechnet
+
+-- 3. Erweiterung:
+-- - Mixtur aus zwei Duschprodukten, bel. Anteile
+-- Funktion entsprechend anpassen
