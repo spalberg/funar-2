63384d2b7505fa6003db7ab7e0016b5e81468175
Sven Palberg <sven@palberg.de>
2023-01-17 10:21:57 +0000

    changes

--- haskell-code/Intro.hs
+++ haskell-code/Intro.hs
@@ -1,4 +1,88 @@
 {-# LANGUAGE InstanceSigs #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module Intro where
 
-import Prelude hiding (Semigroup, Monoid)
+import Prelude hiding (Monoid, Semigroup)
+
+x :: Integer
+x = 7
+
+y :: Integer
+-- >>> y
+-- 97
+y = x * 12 + 13
+
+f :: Integer -> Integer
+f = \n -> n * 2
+
+-- Abkürzung
+f' :: Integer -> Integer
+f' 42 = 42
+f' n = n * 2
+
+-- Ein Haustier ist eins der folgenden
+-- - Katze
+-- - Hund
+-- - Schlange
+
+-- >>> Cat
+-- Cat
+data Pet = Cat | Dog | Snake
+  deriving (Show, Eq)
+
+eqPet :: Pet -> Pet -> Bool
+eqPet Cat Cat = True
+eqPet Dog Dog = True
+eqPet Snake Snake = True
+eqPet _ _ = False
+
+eqCat = eqPet Cat
+
+-- Highway
+data Liveness = Alive | Dead
+  deriving (Show, Eq)
+
+type Weight = Integer
+
+-- data Dillo = MkDillo {dilloLiveness :: Liveness, dilloWeight :: Weight} deriving (Show, Eq)
+
+dillo1 = MkDillo Alive 5
+
+dillo2 = MkDillo Dead 8
+
+runOverDillo dillo = dillo {dilloLiveness = Dead}
+
+-- ADTs
+
+type Sentence = String
+
+data Animal
+  = MkDillo {dilloLiveness :: Liveness, dilloWeight :: Weight}
+  | MkParrot Sentence Weight
+  deriving (Show, Eq)
+
+parrot1 = MkParrot "Hello" 3
+
+parrot2 = MkParrot "Bye" 1
+
+-- Tiere überfahren
+runOverAnimal :: Animal -> Animal
+runOverAnimal (MkDillo _ weight) = MkDillo Dead weight
+runOverAnimal (MkParrot _ weight) = MkParrot "" weight
+
+-- Tiere füttern
+-- >>> feedAnimal (MkDillo Alive 2) 10
+-- MkDillo {dilloLiveness = Alive, dilloWeight = 12}
+feedAnimal :: Animal -> Weight -> Animal
+feedAnimal dillo@(MkDillo liveness weight) amount =
+  case liveness of
+    Dead -> dillo
+    Alive -> MkDillo Alive (weight + amount)
+feedAnimal (MkParrot sentence weight) amount = MkParrot sentence (weight + amount)
+
+-- tuplify
+tuplify :: (a -> b -> c) -> ((a, b) -> c)
+tuplify f = \ (a, b) -> f a b
+
+-- Duschprodukt
