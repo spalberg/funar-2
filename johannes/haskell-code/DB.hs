fb1533cef8d0c64c6d8525fef8d78de5aabd9ee1
Johannes Maier <johannes.maier@active-group.de>
2023-01-18 09:51:36 +0100

    update

--- johannes/haskell-code/DB.hs
+++ johannes/haskell-code/DB.hs
@@ -13,19 +13,27 @@ module DB where
     return (show (x + y))
 -}
 
-data DBCommand a =
-    Put String Integer
-    | Get String
-    | Return a
-    deriving Show
+-- data DBCommand a =
+--     Put String Integer
+--     | Get String
+--     | Return a
+--     deriving Show
 
 -- >>> :t Get "johannes"
 -- Get "johannes" :: DBCommand
 
 -- DB-Programm ist Liste/Abfolge von Commands
-type DBProgram = [DBCommand]
+-- type DBProgram = [DBCommand]
 
-p1 = [ Put "Johannes" 36
-     , Get "Johannes" -- wo ist der Rückgabewert? bzw.
-     -- wie gebe ich ihm einen Namen?
-     ]
+-- p1 = [ Put "Johannes" 36
+--      , Get "Johannes" -- wo ist der Rückgabewert? bzw.
+--      -- wie gebe ich ihm einen Namen?
+--      ]
+
+-- -> führt noch nicht zum Ziel
+
+
+data DBCommand a
+    -- Idee: Callback, um Programm fortzusetzen
+    -- Was ist, wenn wir nichts finden?
+    = Get String (Integer -> a)
