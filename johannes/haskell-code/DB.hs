8b4fd8170c8cb33a24e0bbb21fcdd0c8bf405066
Johannes Maier <johannes.maier@active-group.de>
2023-01-18 09:45:32 +0100

    update

---
+++ johannes/haskell-code/DB.hs
@@ -0,0 +1,19 @@
+module DB where
+
+-- Datenbank: Key-Value-Store
+-- Key: String
+-- Wert: Integer
+
+-- Wunschdenken:
+{-
+    put "Johannes" 36
+    x = get "Johannes"
+    put "Johannes" (x + 1)
+    y = get "Johannes"
+    return (show (x + y))
+-}
+
+data DBCommand =
+    Put String Integer
+    | Get String
+    | Return String
