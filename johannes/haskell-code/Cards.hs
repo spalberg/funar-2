9e24e6e4875d957bf3ae665c3712dbd5ea1441b5
Johannes Maier <johannes.maier@active-group.de>
2023-01-18 14:57:39 +0100

    update

--- johannes/haskell-code/Cards.hs
+++ johannes/haskell-code/Cards.hs
@@ -34,7 +34,7 @@ data Card = MkCard Suit Rank
 
 allCards :: [Card]
 allCards = -- [ MkCard suit rank | suit <- allSuits, rank <- allRanks ] -- list comprehension
-    cartesianProduct allSuits allRanks
+    fmap MkCard (cartesianProduct allSuits allRanks)
 
 cartesianProduct :: [a] -> [b] -> [(a, b)]
 cartesianProduct list1 list2 =
