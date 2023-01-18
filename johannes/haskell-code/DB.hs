c061ce01af75c5927b3d2cea0e01a50db8f947a4
Johannes Maier <johannes.maier@active-group.de>
2023-01-18 11:47:05 +0100

    update

--- johannes/haskell-code/DB.hs
+++ johannes/haskell-code/DB.hs
@@ -128,5 +128,5 @@ splice (Get key callback) f =
 
     -- brauchen hier: _ein konkretes_ DB b
     -- Michael: woher kriegt man den Integer?
-    Get key (\ i -> callback i)
+    Get key (\ i -> splice (callback i) f)
 splice _ _ = undefined
