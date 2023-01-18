4d3c63d5c749f4c5d1eb26db8b9cae0f1084b921
Sven Palberg <sven@palberg.de>
2023-01-18 11:18:55 +0000

    Add functor for db

---
+++ haskell-code/DB.hs
@@ -0,0 +1,97 @@
+{-# LANGUAGE InstanceSigs #-}
+module DB where
+
+-- <- Alias
+import Data.Map (Map, (!))
+import qualified Data.Map as Map
+
+-- Datenbank: Key-Value-Store
+-- Key: String
+-- Wert: Integer
+
+-- Wunschdenken
+{-
+    put "Sven" 27
+    x = get "Sven"
+    put "Sven" (x + 1)
+    y = get "Sven"
+    return (show (x + y))
+-}
+
+-- data DBCommand a
+--   = Put String Integer
+--   | Get String
+--   | Return a
+--   deriving (Show)
+
+-- type DBProgramm = [DBCommand]
+
+--
+data DB a
+  = Get String (Integer -> DB a)
+  | Put String Integer (() -> DB a)
+  | Return a
+
+p1 :: DB String
+p1 =
+  Put
+    "Sven"
+    27
+    ( \() ->
+        Get
+          "Sven"
+          ( \x ->
+              Put
+                "Sven"
+                (x + 1)
+                ( \() ->
+                    Get
+                      "Sven"
+                      ( \y ->
+                          Return (show (x + y))
+                      )
+                )
+          )
+    )
+
+get :: String -> DB Integer
+get key = Get key Return
+
+put :: String -> Integer -> DB ()
+put key value = Put key value Return
+
+return :: a -> DB a
+return = Return
+
+-- >>> get "Sven"
+-- No instance for (Show (DB Integer))
+--   arising from a use of ‘evalPrint’
+
+runDB :: Map String Integer -> DB a -> (a, Map String Integer)
+runDB mp (Get key cb) =
+  let value = mp ! key
+   in runDB mp (cb value)
+runDB mp (Put key value cb) =
+  let mp' = Map.insert key value mp
+   in runDB mp' (cb ())
+runDB mp (Return a) = (a, mp)
+
+-- >>> runDB Map.empty p1
+-- ("55",fromList [("Sven",28)])
+
+splice :: DB a -> (a -> DB b) -> DB b
+splice (Return a) f = f a
+splice (Get key cb) f =
+  Get key (\i -> splice (cb i) f)
+splice (Put key value cb) f =
+  Put key value (\_ -> splice (cb ()) f)
+
+-- instance Monoid a => Monad (DB a) where
+--     return = DB.return
+--     (>>=) = splice
+
+instance Functor DB where
+    fmap :: (a -> b) -> DB a -> DB b
+    fmap f (Return a) = Return (f a)
+    fmap f (Get key cb) = Get key (\ i -> fmap f (cb i))
+    fmap f (Put key value cb) = Put key value (\ _ -> fmap f (cb ()))
