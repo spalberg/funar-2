4d876e103aaec48e9b69e0dc68ea66d681612db4
Sven Palberg <sven@palberg.de>
2023-01-19 08:29:16 +0000

    Events and commands

--- hearts/src/GameEvent.hs
+++ hearts/src/GameEvent.hs
@@ -1,4 +1,16 @@
 module GameEvent where
 
 import Cards
+import Data.Map
 
+data GameEvent
+  = HandDealt (Player, Hand)
+  | PlayerTurnChanged Player
+  | LegalCardPlayed Player Card
+  | TrickTaken Player Trick
+  | GameEnded Player
+  | IllegalCardAttempted Player Card
+
+data GameCommand
+  = PlayCard Player Card
+  | DealHands (Map Player Hand)
